aaf564fa08e37277e13fa078a95de35b8d32b638
Ramsey Chapin <ramsey@ramseychapin.com>
2019-11-10 00:42:04 +0000

    Automatically backed up by Learn

--- lib/create.sql
+++ lib/create.sql
@@ -1,13 +1,16 @@
 CREATE TABLE projects (
   id INTEGER PRIMARY KEY,
-  name TEXT,
-  age INTEGER
-
+  title TEXT,
+  category TEXT,
+  funding_goal REAL,
+  start_date TEXT,
+  end_date TEXT,
 );
 
 CREATE TABLE users (
-  
-
+  id INTEGER PRIMARY KEY,
+  name TEXT,
+  age INTEGER
 );
 
 CREATE TABLE pledges (
